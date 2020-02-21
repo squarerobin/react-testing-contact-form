4ab31c46ea34e8f1c3e4b7194eb2d5c21ca1136b
squarerobin <squarerobin@users.noreply.github.com>
2020-02-21 13:20:19 +0100

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, queryByText, getByTestId, getByPlaceholderText } from "@testing-library/react";
+import "mutationobserver-shim";
 import App from "./App";
+import ContactForm from "./components/ContactForm"
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+
+it('renders "First Name" text', () => {
+  const { queryByText } = render(
+    <input
+      name="firstName"
+      placeholder="bill"
+      required
+    />
+  );
+  const firstNameText = queryByText(/First Name/i);
+});
+
+it('input for first name required" text', () => {
+  const firstNameInput = render(
+    <App />);
+  expect(firstNameInput.getByPlaceholderText("bill")).toBeRequired();
+});
+
+it('input for last name required" text', () => {
+ const lastNameInput = render(<App />);
+   expect(lastNameInput.getByPlaceholderText("luo")).toBeRequired();
+
+});
+
+it('input for email required" text', () => {
+  const emailInput = render(<App />);
+  expect(emailInput.getByPlaceholderText("bluebill1049@hotmail.com")).toBeRequired();
+});
+
+it('input for textarea not required" text', () => {
+  const textareaInput = render(<App />);
+expect(textareaInput.getByTestId("textarea")).not.toBeRequired();});
+
+
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios"
 import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
@@ -8,7 +9,21 @@ const ContactForm = () => {
   });
   const onSubmit = data => {
     setData(data);
-  };
+    axios
+      .post("https://reqres.in/api/users", {
+        firstName: "Fred",
+        lastName: "Flintstone"
+      }
+      .then(res) {
+        console.log(res)
+      }
+      .catch(error) {
+        console.log(error)
+      }
+     
+  
+    
+  }
 
   return (
     <div className="App">
@@ -17,8 +32,9 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            placeholder="bill" //bug fixed here: the place holder was in the label above
+            required="required"
+            ref={register({ required: true, maxLength: 12 })} //max length was too short
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -30,6 +46,7 @@ const ContactForm = () => {
           <input
             name="lastName"
             placeholder="luo"
+            required
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -38,17 +55,25 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
-            Email*
-          </label>
-          <input name="email" ref={register({ required: true })} />
+          <label htmlFor="email">Email*</label>
+          <input
+            name="email"
+            placeholder="bluebill1049@hotmail.com"
+            required
+            ref={register({ required: true })}
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            name="message"
+            data-testid="textarea"
+            ref={register({ required: false })}
+            w
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
